9151dc2d7fb2aef61a252c8b81f14c98bf2ba205
Logeshwaran99 <84559800+Logeshwaran99@users.noreply.github.com>
2024-07-13 17:22:16 +0530

    testing

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,12 +1,58 @@
-int isStringEmpty(const char * str)
-{
-  if (str == NULL || str[0] == '\0') {
-        return 1; // The string is empty
+#define MAX_NUMBERS 100
+#define BUFFER_SIZE 100
+
+void split(const char* str, const char* delimiter, char result[MAX_NUMBERS][BUFFER_SIZE], int* count) {
+    char temp[BUFFER_SIZE];
+    strcpy(temp, str);
+
+    char* token = strtok(temp, delimiter);
+    while (token != NULL) {
+        strcpy(result[*count], token);
+        (*count)++;
+        token = strtok(NULL, delimiter);
     }
-return 0;
 }
-int add(const char * str)
-{
-  if(1==isStringEmpty(str))
-    return 0;
+
+const char* parseDelimiter(const char* str, char delimiter[BUFFER_SIZE]) {
+    if (strncmp(str, "//", 2) == 0) {
+        const char* newline = strchr(str, '\n');
+        if (newline != NULL) {
+            int len = newline - str - 2;
+            strncpy(delimiter, str + 2, len);
+            delimiter[len] = '\0';
+            return newline + 1;
+        }
+    }
+    strcpy(delimiter, ",\n");
+    return str;
+}
+
+int calculateSum(const char* str, const char* delimiter) {
+    char numbers[MAX_NUMBERS][BUFFER_SIZE];
+    int count = 0;
+    split(str, delimiter, numbers, &count);
+
+    int sum = 0;
+    int negatives[MAX_NUMBERS];
+    int neg_count = 0;
+
+    for (int i = 0; i < count; i++) {
+        int number = atoi(numbers[i]);
+        if (number < 0) {
+            negatives[neg_count++] = number;
+        } else if (number <= 1000) {
+            sum += number;
+        }
+    }
+    return sum;
+}
+
+int add(const char* str) {
+    if (str == NULL || *str == '\0') {
+        return 0;
+    }
+
+    char delimiter[BUFFER_SIZE];
+    str = parseDelimiter(str, delimiter);
+    return calculateSum(str, delimiter);
 }
