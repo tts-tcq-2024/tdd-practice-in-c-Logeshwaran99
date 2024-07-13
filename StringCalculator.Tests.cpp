ca9129b172c0b1bea2243a8a73a2d4957caa7daf
Logeshwaran99 <84559800+Logeshwaran99@users.noreply.github.com>
2024-07-13 15:45:49 +0530

    Testcase2

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -1,12 +1,12 @@
 #include <gtest/gtest.h>
 #include "StringCalculator.h"
 
-/*TEST(StringCalculatorAddTests, ExpectZeroForEmptyInput) {
+TEST(StringCalculatorAddTests, ExpectZeroForEmptyInput) {
     int expectedresult = 0;
     const char* input = "Hello, world!";
     int result = add(input);
     ASSERT_EQ(result, expectedresult);
-}*/
+}
 
 TEST(StringCalculatorAddTests, ExpectZeroForSingleZero) {
     int expectedresult = 0;
