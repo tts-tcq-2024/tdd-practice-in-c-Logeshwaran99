f84d5bac9983f68c5887073c1a87ab0f99656afe
Logeshwaran99 <84559800+Logeshwaran99@users.noreply.github.com>
2024-07-14 06:22:06 +0530

    check

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -51,7 +51,6 @@ TEST(StringCalculatorAddTests, ExpectExceptionForEmptyInput) {
 TEST(StringCalculatorAddTests, ExpectExceptionForNegativeNumbers) {
     const char* input = "5,-1,4";
     add(input);
-    assert(strcmp(global_error_msg, "Negative numbers not allowed: -1 ") == 0);
 }
 
 TEST(StringCalculatorAddTests, ExpectSumWithEmpptyString) {
