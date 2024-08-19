adbb98af782e922e260ad0cb32a5e851eb136309
Logeshwaran99 <84559800+Logeshwaran99@users.noreply.github.com>
2024-08-19 06:40:58 +0530

    testing

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,83 +1,87 @@
-#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-char error_message[128] = "";
+#define ERROR_MESSAGE_SIZE 128
+#define DELIMITER_SIZE 128
 
-int isemptystring(const char* input) {
-    return (input == NULL || input[0] == '\0') ? 1 : 0;
+char error_message[ERROR_MESSAGE_SIZE] = "";
+
+int is_empty_string(const char* input) {
+    return (input == NULL || input[0] == '\0');
 }
 
-int islessthanthousand(const char *input_seg) {    
-    int value_check = atoi(input_seg);
-    return (value_check < 1000) ? value_check : 0;
+int is_less_than_thousand(const char *input_seg) {    
+    int value = atoi(input_seg);
+    return (value < 1000) ? value : 0;
 }
 
-int Checkifnegative(const char* input, char* delimiter) {
+int check_for_negative(const char* input, const char* delimiter) {
     char* dup_input = strdup(input);
-    char* input_seg = strtok(dup_input, delimiter);
-    while (input_seg != NULL) {
-        int value_check = atoi(input_seg);
-        if (value_check < 0) {
+    if (!dup_input) {
+        strcpy(error_message, "Memory allocation error");
+        return -1;
+    }
+
+    char* token = strtok(dup_input, delimiter);
+    while (token != NULL) {
+        int value = atoi(token);
+        if (value < 0) {
             free(dup_input);
             return -1;
         }
-        input_seg = strtok(NULL, delimiter);
+        token = strtok(NULL, delimiter);
     }
+
     free(dup_input);
     return 0;
 }
 
-void appendcustomdelimiter(const char* input, char* delimiter) {
+void append_custom_delimiter(const char* input, char* delimiter) {
     int i = 2; 
-    while (input[i] != '\n') {
-        char temp[2] = {input[i], '\0'}; // Use a null-terminated string
-        strncat(delimiter, temp, 1);
+    while (input[i] != '\n' && input[i] != '\0') {
+        strncat(delimiter, &input[i], 1);
         i++;
     }
 }
 
-void checkcustomdelimiter(const char* input, char* delimiter) {
+void check_custom_delimiter(const char* input, char* delimiter) {
     if (input[0] == '/' && input[1] == '/') {
-        strcpy(delimiter, "");
-        appendcustomdelimiter(input, delimiter);
+        delimiter[0] = '\0';  // Clear the default delimiter
+        append_custom_delimiter(input, delimiter);
     }
 }
 
-int calculatesum(const char* input, char* delimiter) {
+int calculate_sum(const char* input, const char* delimiter) {
     int sum = 0;
     char* dup_input = strdup(input);
-    char* input_seg = strtok(dup_input, delimiter);
-    while (input_seg != NULL) {
-        sum += islessthanthousand(input_seg);
-        input_seg = strtok(NULL, delimiter);
+    if (!dup_input) {
+        strcpy(error_message, "Memory allocation error");
+        return -1;
     }
+
+    char* token = strtok(dup_input, delimiter);
+    while (token != NULL) {
+        sum += is_less_than_thousand(token);
+        token = strtok(NULL, delimiter);
+    }
+
     free(dup_input);
     return sum;
 }
 
 int add(const char* input) {
-    char delimiter[128] = ",\n";
-    if (isemptystring(input)) {
+    char delimiter[DELIMITER_SIZE] = ",\n";
+
+    if (is_empty_string(input)) {
         return 0;
     }
-    checkcustomdelimiter(input, delimiter);
-    if (Checkifnegative(input, delimiter) == -1) {
-        strcpy(error_message, "Negatives not allowed");
+
+    check_custom_delimiter(input, delimiter);
+
+    if (check_for_negative(input, delimiter) == -1) {
+        strcpy(error_message, "Negative values are not allowed");
         return -1;
     }
-    return calculatesum(input, delimiter);
-}
 
-int main() {
-    const char* testStr = "//;\n1;2;-3;4";
-    int result = add(testStr);
-
-    if (result == -1) {
-        printf("%s\n", error_message);
-    } else {
-        printf("Sum: %d\n", result);
-    }
-
-    return 0;
+    return calculate_sum(input, delimiter);
 }
