6b2454a9b70900d12ede2c73c91778b41b39e0af
Logeshwaran99 <84559800+Logeshwaran99@users.noreply.github.com>
2024-08-19 05:51:18 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,75 +1,143 @@
-#define MAX_NUMBERS 100
-#define BUFFER_SIZE 100
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
-void split(const char* str, const char* delimiter, char result[MAX_NUMBERS][BUFFER_SIZE], int* count) {
-    char temp[BUFFER_SIZE];
-    strcpy(temp, str);
+bool is_custom_delimiter(const char* input) {
+    return (input[0] == '/' && input[1] == '/') || (input[0] == '\n');
+}
 
-    char* token = strtok(temp, delimiter);
-    while (token != NULL) {
-        strcpy(result[*count], token);
-        (*count)++;
+void find_custom_delimiter(const char* input, const char** start_pos, const char** end_pos) {
+    if (input[0] == '/') {
+        *start_pos = input + 2; // Skip over '//'
+        *end_pos = strchr(*start_pos, '\n');
+    } else if (input[0] == '\n') {
+        *start_pos = input + 1; // Skip '\n'
+        *end_pos = NULL; // No delimiter after '\n'
+    }
+}
+
+void extract_custom_delimiter(const char* input, char* delimiter) {
+    const char* start_pos;
+    const char* end_pos;
+    find_custom_delimiter(input, &start_pos, &end_pos);
+
+    if (end_pos) {
+        strncpy(delimiter, start_pos, end_pos - start_pos);
+        delimiter[end_pos - start_pos] = '\0';
+    } else {
+        strcpy(delimiter, ",");
+    }
+}
+
+const char* skip_double_slash(const char* input) {
+    if (input[0] == '/' && input[1] == '/') {
+        return strchr(input, '\n') + 1; // Skip over '//'
+    }
+    return input;
+}
+
+const char* skip_newline(const char* input) {
+    if (input[0] == '\n') {
+        return input + 1; // Skip '\n'
+    }
+    return input;
+}
+
+const char* get_start_position(const char* input) {
+    return skip_double_slash(skip_newline(input));
+}
+
+void preprocess_input(char* str) {
+    while (*str) {
+        if (*str == '\n') {
+            *str = ','; // Replace '\n' with ','
+        }
+        str++;
+    }
+}
+
+void extract_numbers(const char* input, char* numbers) {
+    const char* start_pos = get_start_position(input);
+    strcpy(numbers, start_pos);
+    preprocess_input(numbers); // Replace '\n' with ','
+}
+
+void parse_input(const char* input, char* delimiter, char* numbers) {
+    if (is_custom_delimiter(input)) {
+        extract_custom_delimiter(input, delimiter);
+    } else {
+        strcpy(delimiter, ",");
+    }
+    extract_numbers(input, numbers);
+}
+
+void split_numbers(const char* str, const char* delimiter, int* numbers, int* count) {
+    char* copy_str = strdup(str);
+    char* token = strtok(copy_str, delimiter);
+    while (token) {
+        numbers[(*count)++] = atoi(token);
         token = strtok(NULL, delimiter);
     }
+    free(copy_str);
 }
 
-const char* parseDelimiter(const char* str, char delimiter[BUFFER_SIZE]) {
-    if (strncmp(str, "//", 2) == 0) {
-        const char* newline = strchr(str, '\n');
-        if (newline != NULL) {
-            int len = newline - str - 2;
-            strncpy(delimiter, str + 2, len);
-            delimiter[len] = '\0';
-            return newline + 1;
+bool has_negatives(int* numbers, int size) {
+    for (int i = 0; i < size; i++) {
+        if (numbers[i] < 0) {
+            return true;
         }
     }
-    strcpy(delimiter, ",\n");
-    return str;
+    return false;
 }
 
-int processNumber(const char* numStr, int* negatives, int* neg_count) {
-    int number = atoi(numStr);
-    if (number < 0) {
-        negatives[(*neg_count)++] = number;
-    }
-    return (number <= 1000) ? number : 0;
-}
-
-void handleNegatives(int negatives[], int neg_count) { // it can be used for validation
-    if (neg_count > 0) {
-        printf("Negatives not allowed: ");
-        for (int i = 0; i < neg_count; i++) {
-            printf("%d ", negatives[i]);
+void construct_negative_message(int* numbers, int size, char* message) {
+    strcpy(message, "negatives not allowed: ");
+    for (int i = 0; i < size; i++) {
+        if (numbers[i] < 0) {
+            char num_str[12];
+            snprintf(num_str, sizeof(num_str), "%d", numbers[i]);
+            strcat(message, num_str);
+            strcat(message, " ");
         }
-        printf("\n");
-        //exit(1); // displays the negative strings and comes out of the code execution
     }
 }
 
-int calculateSum(char numbers[MAX_NUMBERS][BUFFER_SIZE], int count) {
+void check_negatives(int* numbers, int size) {
+    if (has_negatives(numbers, size)) {
+        char message[256];
+        construct_negative_message(numbers, size, message);
+       printf("%s\n", message);
+       // exit(EXIT_FAILURE);
+    }
+}
+
+void sum_valid_numbers(int* num_array, int num_count, int* sum) {
+    for (int i = 0; i < num_count; i++) {
+        if (num_array[i] <= 1000) {
+            *sum += num_array[i];
+        }
+    }
+}
+
+int add(const char* input) {
+if (*input == '\0') {
+    return 0;
+}
+
+
+    char delimiter[10] = {0};
+    char numbers[1000] = {0};
+    parse_input(input, delimiter, numbers);
+
+    int num_array[1000];
+    int num_count = 0;
+    split_numbers(numbers, delimiter, num_array, &num_count);
+
+    check_negatives(num_array, num_count);
+
     int sum = 0;
-    int negatives[MAX_NUMBERS];
-    int neg_count = 0;
+    sum_valid_numbers(num_array, num_count, &sum);
 
-    for (int i = 0; i < count; i++) {
-        sum += processNumber(numbers[i], negatives, &neg_count);
-    }
-    handleNegatives(negatives, neg_count); //can be done but for negative cases in runtime error will be produced and so build is failing
     return sum;
-}
-
-int add(const char* str) {
-    if (strlen(str) == 0) {
-        return 0;
-    }
-
-    char delimiter[BUFFER_SIZE];
-    str = parseDelimiter(str, delimiter);
-
-    char numbers[MAX_NUMBERS][BUFFER_SIZE];
-    int count = 0;
-    split(str, delimiter, numbers, &count);
-
-    return calculateSum(numbers, count);
-    
 }
